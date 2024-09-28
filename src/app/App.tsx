9c63bcdce1af57f2adc0a0cc2519f17033aaa31e
jinseunghyeon <jinsh9050@gmail.com>
2024-09-28 17:21:31 +0900

    init

---
+++ src/app/App.tsx
@@ -0,0 +1,23 @@
+/** @jsxImportSource @emotion/react */
+import "./reset.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1
+          css={{
+            fontSize: "2rem",
+          }}
+        >
+          My App
+        </h1>
+      </header>
+      <main>
+        <p>Welcome to my app!</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
